fe55e078ddacc4a575bbee1e403f5061ca272635
Luan <luancostar@gmail.com>
2025-05-14 16:01:50 -0300

    Update

--- components/features.tsx
+++ components/features.tsx
@@ -40,7 +40,7 @@ export default function Features() {
               </span>
             </div>
             <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Desfrute de indicadores modernos e acertivos
+              Possua indicadores modernos e acertivos
             </h2>
             <p className="text-lg text-indigo-200/65">
               Através de indicadores totalmente personalizáveis, desfrute de dashboards poderosos, acompanhe performances em tempo real e tenha tomadas de decisões ainda mais performáticas.
@@ -71,11 +71,10 @@ export default function Features() {
                 />
               </svg>
               <h3 className="mb-1 font-nacelle text-[1rem] font-semibold text-gray-200">
-                Project Milestones
+                Totalmente Responsivo
               </h3>
               <p className="text-indigo-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                O <strong>zylogi</strong> está disponível para ser utilizado da forma mais dinâmica e inteligente possível. Seja no seu desktop, notebook, celular ou tablet. Ambiente 100% responsivo.
               </p>
             </article>
             <article>
@@ -89,11 +88,10 @@ export default function Features() {
                 <path d="M19 6H0v2h17v8H7v-6H5v8h19v-2h-5V6Z" />
               </svg>
               <h3 className="mb-1 font-nacelle text-[1rem] font-semibold text-gray-200">
-                Team Views
+                 Rotas inteligentes e personalizáveis
               </h3>
               <p className="text-indigo-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Rotas de coleta e entrega totalmente automatizadas. Roteirização inteligente, trazendo rotas dinâmicas em um ambiente totalmente intuitivo.
               </p>
             </article>
             <article>
@@ -110,11 +108,11 @@ export default function Features() {
                 />
               </svg>
               <h3 className="mb-1 font-nacelle text-[1rem] font-semibold text-gray-200">
-                Advanced Search
+                Zylogi Driver App
               </h3>
               <p className="text-indigo-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Monitore suas encomendas e entregas através de aplicativo inteligente. Aplicativo para o uso seu motorista parceiro. Receba anexos, status de ocorrências e observações valiosas para tornar
+                suas informações ainda mais precisas
               </p>
             </article>
             <article>
@@ -136,11 +134,10 @@ export default function Features() {
                 <path d="m16.321 2-.5-.866 1.733-1 .5.866A22 22 0 0 1 21 12c0 3.852-1.017 7.636-2.948 10.97l-.502.865-1.73-1.003.501-.865A19.878 19.878 0 0 0 19 12a20 20 0 0 0-2.679-10Z" />
               </svg>
               <h3 className="mb-1 font-nacelle text-[1rem] font-semibold text-gray-200">
-                Strategic Initiatives
+                Comunicação Estratégica
               </h3>
               <p className="text-indigo-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                 Trate pendências em tempo real! Chat integrado e sistema de gerenciamento de pendências em tempo real! Esteja sempre alinhado com a posição de sua carga
               </p>
             </article>
             <article>
@@ -157,11 +154,10 @@ export default function Features() {
                 <path d="m7.454 2.891.891-.454L7.437.655l-.891.454a12 12 0 0 0 0 21.382l.89.454.91-1.781-.892-.455a10 10 0 0 1 0-17.818ZM17.456 1.11l-.891-.454-.909 1.782.891.454a10 10 0 0 1 0 17.819l-.89.454.908 1.781.89-.454a12 12 0 0 0 0-21.382Z" />
               </svg>
               <h3 className="mb-1 font-nacelle text-[1rem] font-semibold text-gray-200">
-                Flexible Workflows
+                Flexibilidade Garantida
               </h3>
               <p className="text-indigo-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                o <strong>zylogi</strong> emite todo tipo de documentação também pelo seu smartphone. Operação dinâmica, presente e resolutiva. Poupe tempo, ganhe em performance.
               </p>
             </article>
             <article>
@@ -178,11 +174,10 @@ export default function Features() {
                 <path d="M19.406 3.844 6.083 20.497.586 15 2 13.586l3.917 3.917L17.844 2.595l1.562 1.25Z" />
               </svg>
               <h3 className="mb-1 font-nacelle text-[1rem] font-semibold text-gray-200">
-                Unified Timeline
+                Zylogi Client App
               </h3>
               <p className="text-indigo-200/65">
-                Track progress across custom flows for your team. Find the right
-                balance for the user, privacy and security.
+                Mais que um simples portal para o cliente. Aqui, seu cliente consegue verificar posicionamento de suas encomendas, coletas, histórico de solicitações, performance e relatórios detalhados! Além de solicitações de coleta de forma autónoma.
               </p>
             </article>
           </div>
